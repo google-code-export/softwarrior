97ffc30468a01b8509a4309958b6502f35654375
wise4man@gmail.com <wise4man@gmail.com@7157628a-559a-e2cc-37fc-09edb6edd1f7>
2011-02-08 12:22:05 +0000

    added rss delete lib

--- trunk/RutrackerDownloader/src/com/softwarrior/rutrackerdownloader/RutrackerDownloader.java
+++ trunk/RutrackerDownloader/src/com/softwarrior/rutrackerdownloader/RutrackerDownloader.java
@@ -1,13 +1,31 @@
 package com.softwarrior.rutrackerdownloader;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
+
+import com.softwarrior.rss.MessageList;
 
 public class RutrackerDownloader extends Activity {
-    /** Called when the activity is first created. */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        intent.setClassName(this, MessageList.class.getName());
+        startActivity(intent);
     }
+    
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+    	if(keyCode == KeyEvent.KEYCODE_MENU) {
+            return true;
+    	}
+    	return super.onKeyDown(keyCode, event);
+    }
+
+    
 }
