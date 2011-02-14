4836fc6ab4f5b1d2f62ed6294612f64c623f8c12
wise4man@gmail.com <wise4man@gmail.com@7157628a-559a-e2cc-37fc-09edb6edd1f7>
2011-02-14 20:12:51 +0000

    added progress bar and interface for proxy

--- trunk/RutrackerDownloader/jni/libtorrent.h
+++ trunk/RutrackerDownloader/jni/libtorrent.h
@@ -1,4 +1,5 @@
 #include <jni.h>
+#include <android/log.h>
 
 #ifndef __Included_Java_com_softwarrior_libtorrent_LibTorrent__
 #define __Included_Java_com_softwarrior_libtorrent_LibTorrent__
@@ -7,8 +8,14 @@
 extern "C" {
 #endif
 
+#define LOG_TAG "Softwarrior"
+#define LOG_INFO(...) {__android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__);}
+
 JNIEXPORT jstring JNICALL Java_com_softwarrior_libtorrent_LibTorrent_StartDownload
-	(JNIEnv *, jobject, jstring, jstring, jint, jint);
+	(JNIEnv *, jobject, jstring, jstring, jint, jint, jstring, jint, jstring, jstring);
+
+JNIEXPORT jint JNICALL Java_com_softwarrior_libtorrent_LibTorrent_GetProgress
+  (JNIEnv *, jobject);
 
 JNIEXPORT jbyteArray JNICALL Java_com_softwarrior_libtorrent_LibTorrent_StopDownload
 	(JNIEnv *, jobject);
