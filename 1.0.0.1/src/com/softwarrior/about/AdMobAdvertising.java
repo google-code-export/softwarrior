2add4a25a4b1fb584f7a38858f608bee3401f463
wise4man@gmail.com <wise4man@gmail.com@7157628a-559a-e2cc-37fc-09edb6edd1f7>
2011-03-28 00:55:35 +0000

    move

---
+++ 1.0.0.1/src/com/softwarrior/about/AdMobAdvertising.java
@@ -0,0 +1,180 @@
+package com.softwarrior.about;
+
+import com.admob.android.ads.AdListener;
+import com.admob.android.ads.AdManager;
+import com.admob.android.ads.AdView;
+import com.admob.android.ads.InterstitialAd;
+import com.admob.android.ads.InterstitialAdListener;
+import com.admob.android.ads.SimpleAdListener;
+import com.admob.android.ads.InterstitialAd.Event;
+
+import com.softwarrior.rutrackerdownloader.FullWakeActivity;
+import com.softwarrior.rutrackerdownloader.R;
+import com.softwarrior.rutrackerdownloader.RutrackerDownloaderApp;
+import com.softwarrior.rutrackerdownloader.RutrackerDownloaderApp.ActivityResultType;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class AdMobAdvertising extends FullWakeActivity implements AdListener, InterstitialAdListener {
+
+	  //AdMob 
+	  private InterstitialAd mInterstitialAd;
+	  private AdView 		 mAdView;
+	  
+		public enum MenuType{
+			About, Help, FileManager, Exit;
+		}
+
+	   @Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.admob_advertising);
+
+//	        AdManager.setPublisherId("a14d5a500187b19");
+	        AdManager.setTestDevices(new String[] { AdManager.TEST_EMULATOR, "92D0B17743FC28D496804E97F99B6D10" });        
+	        AdManager.setTestAction("video_int");
+
+	        mAdView = (AdView) findViewById(R.id.ad);
+	        mAdView.setAdListener(new AdvertisingListener());
+	        
+	        // call for an Interstitial Ad
+	        mInterstitialAd = new InterstitialAd(Event.APP_START, this);
+	        mInterstitialAd.requestAd(this);
+	        	        
+	        if(RutrackerDownloaderApp.ExitState) RutrackerDownloaderApp.FinalCloseApplication(this);
+		    RutrackerDownloaderApp.AnalyticsTracker.trackPageView("/Advertising");
+	    }
+
+	   @Override
+		public boolean onCreateOptionsMenu(Menu menu) {
+			super.onCreateOptionsMenu(menu);
+			menu.add(Menu.NONE, MenuType.About.ordinal(), MenuType.About.ordinal(), R.string.menu_about); 
+			menu.add(Menu.NONE, MenuType.Help.ordinal(), MenuType.Help.ordinal(), R.string.menu_help); 
+			menu.add(Menu.NONE, MenuType.FileManager.ordinal(), MenuType.FileManager.ordinal(), R.string.menu_file_manager); 
+			menu.add(Menu.NONE, MenuType.Exit.ordinal(), MenuType.Exit.ordinal(), R.string.menu_exit);
+			return true;
+		}
+		
+		@Override
+		public boolean onMenuItemSelected(int featureId, MenuItem item) {
+			super.onMenuItemSelected(featureId, item);
+			MenuType type = MenuType.values()[item.getItemId()];
+			switch(type)
+			{
+			case About:{
+				RutrackerDownloaderApp.AboutActivity(this);
+			} break;
+			case Help:{
+				RutrackerDownloaderApp.HelpActivity(this);
+			} break;
+			case FileManager:{
+				RutrackerDownloaderApp.FileManagerActivity(this);
+			} break;
+			case Exit:{
+				RutrackerDownloaderApp.FinalCloseApplication(this);
+			} break;
+			}
+			return true;
+		}
+	   	
+		@Override 
+		public boolean onKeyDown(int keyCode, KeyEvent event) {
+			if(keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0)
+				RutrackerDownloaderApp.FinalCloseApplication(this);
+			return super.onKeyDown(keyCode,event); 
+		}
+		
+	    @Override
+	    protected void onResume() {
+	    	super.onResume();
+	        if(RutrackerDownloaderApp.ExitState) RutrackerDownloaderApp.FinalCloseApplication(this);
+	    }
+	    
+		@Override
+		protected void onDestroy() {
+			super.onDestroy();
+			//RutrackerDownloaderApp.AnalyticsTracker.dispatch();
+		}
+		
+        //TODO: Need to show animation picture with girl
+		
+	    //If we fail to receive an interstitial ad, we just keep going on with our application loading and execution.
+		public void onFailedToReceiveInterstitial(InterstitialAd interstitialAd) {
+	      // we couldn't get an interstitial ad before the start. to do ...
+	      if (interstitialAd == mInterstitialAd) {
+	    	//Back to AdView showing
+	      }
+	    }
+
+	    //If we get an interstitial ad successfully, we can show the ad. 
+		public void onReceiveInterstitial(InterstitialAd interstitialAd) {
+	      if(interstitialAd == mInterstitialAd) {
+	        mInterstitialAd.show(this);
+	      }
+	    }
+				
+	    //After the ad has been shown, it will return with an activity result where
+	    //ADMOB_INTENT_BOOLEAN is true.  Once you receive this result you can continue
+	    //application loading and execution.
+	    @Override
+	    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+			switch(ActivityResultType.getValue(resultCode))
+			{
+			case RESULT_DOWNLOADER:
+				RutrackerDownloaderApp.OpenDownloaderActivity(this);
+				return;
+			case RESULT_PREFERENCES:
+				return;
+			case RESULT_EXIT:
+				RutrackerDownloaderApp.FinalCloseApplication(this);
+				return;
+			};
+	    	if(data != null && data.getBooleanExtra(InterstitialAd.ADMOB_INTENT_BOOLEAN, false)) {
+	    		//Back to AdView showing
+	    	}
+	    }
+	    	    	    		
+		public void OnClickButtonRefreshAdvertising(View v) {			
+			mAdView.requestFreshAd();
+			mInterstitialAd = new InterstitialAd(Event.SCREEN_CHANGE, this);
+			mInterstitialAd.requestAd(this); //request an ad now so it's ready when we want to show it			
+		}  
+  
+		private class AdvertisingListener extends SimpleAdListener {
+			@Override
+			public void onFailedToReceiveAd(AdView adView){
+				super.onFailedToReceiveAd(adView);
+			}		
+			@Override
+			public void onFailedToReceiveRefreshedAd(AdView adView){
+				super.onFailedToReceiveRefreshedAd(adView);
+			}		
+			@Override
+			public void onReceiveAd(AdView adView){
+				super.onReceiveAd(adView);
+			}
+			@Override
+			public void onReceiveRefreshedAd(AdView adView){
+				super.onReceiveRefreshedAd(adView);
+			}			
+		}
+		
+		public void onFailedToReceiveAd(AdView adView) {
+			Log.d(RutrackerDownloaderApp.TAG, "onFailedToReceiveAd");
+		}
+		public void onFailedToReceiveRefreshedAd(AdView adView) {
+			Log.d(RutrackerDownloaderApp.TAG, "onFailedToReceiveRefreshedAd");
+		}
+		public void onReceiveAd(AdView adView){
+			Log.d(RutrackerDownloaderApp.TAG, "onReceiveAd");
+		}
+		public void onReceiveRefreshedAd(AdView adView){
+			Log.d(RutrackerDownloaderApp.TAG, "onReceiveRefreshedAd");
+		}
+}
