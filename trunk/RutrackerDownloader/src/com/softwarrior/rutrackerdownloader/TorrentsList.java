087a9b79a02250d4c1b30aa3da7353a32c8ba922
wise4man@gmail.com <wise4man@gmail.com@7157628a-559a-e2cc-37fc-09edb6edd1f7>
2011-04-11 15:47:57 +0000

    fixed listener

--- trunk/RutrackerDownloader/src/com/softwarrior/rutrackerdownloader/TorrentsList.java
+++ trunk/RutrackerDownloader/src/com/softwarrior/rutrackerdownloader/TorrentsList.java
@@ -4,6 +4,11 @@ import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -21,7 +26,17 @@ import java.io.File;
 import java.io.FileInputStream;
 
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
+import com.google.ads.Ad;
+import com.google.ads.AdListener;
+import com.google.ads.AdRequest;
+import com.google.ads.AdView;
+import com.google.ads.AdRequest.ErrorCode;
+import com.mobclix.android.sdk.MobclixAdView;
+import com.mobclix.android.sdk.MobclixAdViewListener;
+import com.mobclix.android.sdk.MobclixMMABannerXLAdView;
 import com.softwarrior.rutrackerdownloader.RutrackerDownloaderApp.ActivityResultType;
 //----------------------------------------------------------------------------------
 class TorrentContainer{
@@ -37,10 +52,21 @@ class TorrentContainer{
     public long getProgress(){return mProgress;}     
 }
 //----------------------------------------------------------------------------------
-public class TorrentsList extends ListActivity {
+public class TorrentsList extends ListActivity implements AdListener, MobclixAdViewListener {
     static public ArrayList<TorrentContainer> Torrents = new ArrayList<TorrentContainer>();
     
+    private WakeLock mWakeLock;
+    
     TorrentAdapter mAdapter;
+    
+	private Timer mAdRefreshTimer;
+	private static final int mAdRefreshTime = 30000; //30 seconds
+	private Handler mAdRefreshTimerHandler;
+	//Mobclix
+	private MobclixMMABannerXLAdView mAdviewBanner;
+	//AdMob 
+  	private AdView mAdView;
+  	private AdRequest mAdRequest;    
     
     enum MenuType{
     	About, Help, Preferences, FileManager, Exit;
@@ -49,9 +75,15 @@ public class TorrentsList extends ListActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+		PowerManager pm = (PowerManager)getSystemService(POWER_SERVICE);
+		mWakeLock = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK
+			| PowerManager.ACQUIRE_CAUSES_WAKEUP, "SHARED");
+		mWakeLock.setReferenceCounted(false);
+
         // Use a custom layout file
         setContentView(R.layout.torrentslist);
-
+        
         final ListView listView = getListView();
         // Tell the list view which view to display when the list is empty
         listView.setEmptyView(findViewById(R.id.empty));
@@ -70,9 +102,84 @@ public class TorrentsList extends ListActivity {
         mAdapter = new TorrentAdapter(this);
         setListAdapter(mAdapter);        
 
+        //Mobclix
+        mAdviewBanner = (MobclixMMABannerXLAdView) findViewById(R.id.advertising_banner_view);	        
+        //AdMob
+        mAdView = (AdView) findViewById(R.id.adView);
+        
+        Context context = getApplicationContext();
+        if(RutrackerDownloaderApp.CheckMode(context) && RutrackerDownloaderApp.CheckService(context)){
+        	TextView adInfo = (TextView) findViewById(R.id.site_choice_message);
+        	adInfo.setVisibility(View.GONE);
+        	mAdviewBanner.setVisibility(View.GONE);
+        	mAdView.setVisibility(View.GONE);
+        } else{
+	        //Mobclix
+	        mAdviewBanner.addMobclixAdViewListener(this);
+    		mAdviewBanner.getAd();
+	        //AdMob
+    		mAdView.setAdListener(this);
+	        mAdRequest = new AdRequest();
+	        //mAdRequest.setTesting(true);
+	        mAdView.loadAd(mAdRequest);
+
+	        mAdRefreshTimer = new Timer();
+	        mAdRefreshTimer.schedule(new AdRefreshTimerTask(), mAdRefreshTime, mAdRefreshTime);
+	        mAdRefreshTimerHandler = new Handler() {
+	            @Override
+	            public void handleMessage(Message msg) {
+					mAdView.loadAd(mAdRequest);
+	        		mAdviewBanner.getAd();
+	            }
+	        };        
+        }
+        
         if(RutrackerDownloaderApp.ExitState) RutrackerDownloaderApp.CloseApplication(this);
         RutrackerDownloaderApp.AnalyticsTracker.trackPageView("/TorrentsList");
     }
+    
+    @Override
+    protected void onDestroy() {
+		if(mWakeLock.isHeld()) {
+		    Log.w(RutrackerDownloaderApp.TAG, "WakeLock is still held");
+		    mWakeLock.release();
+		}
+		super.onDestroy();
+    }
+  
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mWakeLock.acquire();
+        AddTorrent(RutrackerDownloaderApp.TorrentFullFileName,0);
+        mAdapter.notifyDataSetChanged();
+        if(RutrackerDownloaderApp.ExitState) RutrackerDownloaderApp.CloseApplication(this);
+    }
+    
+    @Override
+    protected void onPause() {
+    	mWakeLock.release();
+    	super.onPause();
+    	if(mAdRefreshTimer != null){
+    		mAdRefreshTimer.cancel(); 
+    		mAdRefreshTimer = null;
+    	}
+    }
+    
+    @Override
+    protected void onRestart() {
+    	super.onRestart();
+    	mAdRefreshTimer = new Timer();
+    	mAdRefreshTimer.schedule(new AdRefreshTimerTask(), 0, mAdRefreshTime);
+    }	    		
+    
+    private class AdRefreshTimerTask extends TimerTask {			
+		@Override
+		public void run() {
+			if(mAdRefreshTimerHandler!=null)
+			    mAdRefreshTimerHandler.sendEmptyMessage(0);
+		}	    	
+    }	   
     
     static public void AddTorrent(String FileName, long progress){
         if(FileName.equals("undefined"))
@@ -99,14 +206,6 @@ public class TorrentsList extends ListActivity {
     public void OnClickButtonRemoveTorrents(View v){
         Torrents.clear();
         mAdapter.notifyDataSetChanged();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        AddTorrent(RutrackerDownloaderApp.TorrentFullFileName,0);
-        mAdapter.notifyDataSetChanged();
-        if(RutrackerDownloaderApp.ExitState) RutrackerDownloaderApp.CloseApplication(this);
     }
             
     @Override
@@ -194,4 +293,41 @@ public class TorrentsList extends ListActivity {
 		}
 		return true;
 	}
+	//Mobclix
+	public String keywords()	{ return null;}
+	public String query()		{ return null;}
+	public void onAdClick(MobclixAdView arg0) {
+		Log.v(RutrackerDownloaderApp.TAG, "Mobclix clicked");
+	}
+	public void onCustomAdTouchThrough(MobclixAdView adView, String string) {
+		Log.v(RutrackerDownloaderApp.TAG, "Mobclix The custom ad responded with '" + string + "' when touched!");
+	}
+	public boolean onOpenAllocationLoad(MobclixAdView adView, int openAllocationCode) {
+		Log.v(RutrackerDownloaderApp.TAG, "Mobclix The ad request returned open allocation code: " + openAllocationCode);
+		return false;
+	}
+	public void onSuccessfulLoad(MobclixAdView view) {
+		Log.v(RutrackerDownloaderApp.TAG, "Mobclix The ad request was successful!");
+		view.setVisibility(View.VISIBLE);
+	}
+	public void onFailedLoad(MobclixAdView view, int errorCode) {
+		Log.v(RutrackerDownloaderApp.TAG, "Mobclix The ad request failed with error code: " + errorCode);
+		view.setVisibility(View.GONE);
+	}
+	//AdMob
+	public void onDismissScreen(Ad ad) {
+		Log.v(RutrackerDownloaderApp.TAG, "AdMob onDismissScreen");
+	}
+	public void onFailedToReceiveAd(Ad ad, ErrorCode errorCode) {
+		Log.v(RutrackerDownloaderApp.TAG, "AdMob failed to receive ad (" + errorCode + ")");			
+	}
+	public void onLeaveApplication(Ad ad) {
+		Log.v(RutrackerDownloaderApp.TAG, "AdMob onLeaveApplication");
+	}
+	public void onPresentScreen(Ad ad) {
+		Log.v(RutrackerDownloaderApp.TAG, "AdMob onLeaveApplication");			
+	}
+	public void onReceiveAd(Ad ad) {
+		Log.v(RutrackerDownloaderApp.TAG, "AdMob onReceiveAd");						
+	}				
 }
