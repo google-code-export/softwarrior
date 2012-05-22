afccc8745bf9c27f5310e46742c920d66780668b
wise4man@gmail.com <wise4man@gmail.com@7157628a-559a-e2cc-37fc-09edb6edd1f7>
2012-05-22 15:13:24 +0000

    added web

---
+++ trunk/KingsAndEmperorsOfRussia/KingsAndEmperorsOfRussia.web/JS/variables.js
@@ -0,0 +1,22 @@
+////////////////////////////////////////////////////////////////////////
+//Variables
+////////////////////////////////////////////////////////////////////////
+var History =
+[[null,null],
+ [null,null]];
+ 
+var Tree =
+[[null,null],
+ [null,null]];
+
+var Arms =
+[[null,null],
+ [null,null]];
+
+var Hymn =
+[[null,null],
+ [null,null]];
+
+var Flag =
+[[null,null],
+ [null,null]];
