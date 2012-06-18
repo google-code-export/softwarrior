48f438e0a2db164fdd357bc21768df37ab7e17d3
wise4man@gmail.com <wise4man@gmail.com@7157628a-559a-e2cc-37fc-09edb6edd1f7>
2012-06-18 16:40:14 +0000

    initial

---
+++ KingsAndEmperorsOfRussia/iOS/KingsAndEmperorsOfRussia/KingsAndEmperorsOfRussia/Debug.h
@@ -0,0 +1,21 @@
+//
+//  Debug.h
+//  KingsAndEmperorsOfRussia
+//
+//  Created by Andrey on 3/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#ifdef DEBUG
+#   define DLog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
+#else
+#   define DLog(...)
+#endif
+#define ALog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
+#ifdef DEBUG
+#   define ULog(fmt, ...)  { UIAlertView *alert = [[UIAlertView alloc] initWithTitle:[NSString stringWithFormat:@"%s\n [Line %d] ", __PRETTY_FUNCTION__, __LINE__] message:[NSString stringWithFormat:fmt, ##__VA_ARGS__]  delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil]; [alert show]; }
+#else
+#   define ULog(...)
+#endif
---
+++ KingsAndEmperorsOfRussia/iOS/KingsAndEmperorsOfRussia/KingsAndEmperorsOfRussia/FilteredWebCache.h
@@ -0,0 +1,15 @@
+//
+//  FilteredWebCache.h
+//  KingsAndEmperorsOfRussia
+//
+//  Created by Andrey on 3/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FilteredWebCache : NSURLCache
+{
+}
+@end
+
