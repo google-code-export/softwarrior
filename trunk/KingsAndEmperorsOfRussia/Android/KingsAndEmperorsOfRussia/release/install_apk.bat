f4abd8424ef17bd2acb04feae5b7ccb83af8239d
wise4man@gmail.com <wise4man@gmail.com@7157628a-559a-e2cc-37fc-09edb6edd1f7>
2012-06-25 16:03:28 +0000

    changed version

--- trunk/KingsAndEmperorsOfRussia/Android/KingsAndEmperorsOfRussia/release/install_apk.bat
+++ trunk/KingsAndEmperorsOfRussia/Android/KingsAndEmperorsOfRussia/release/install_apk.bat
@@ -1,1 +1,1 @@
-adb install -r "D:/SOFTWARRIOR/KingsAndEmperorsOfRussia/Android/KingsAndEmperorsOfRussia/release/KingsAndEmperorsOfRussia_1_0_0_0.apk"
+adb install -r "D:/SOFTWARRIOR/KingsAndEmperorsOfRussia/Android/KingsAndEmperorsOfRussia/release/KingsAndEmperorsOfRussia_1_0_0_1.apk"
