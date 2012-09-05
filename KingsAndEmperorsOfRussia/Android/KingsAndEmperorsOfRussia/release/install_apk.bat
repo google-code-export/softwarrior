ba0864b147079fab6c99fb7f1fbc990ce866ca61
wise4man@gmail.com <wise4man@gmail.com@7157628a-559a-e2cc-37fc-09edb6edd1f7>
2012-09-05 18:18:31 +0000

    export to sencha touch 2

--- KingsAndEmperorsOfRussia/Android/KingsAndEmperorsOfRussia/release/install_apk.bat
+++ KingsAndEmperorsOfRussia/Android/KingsAndEmperorsOfRussia/release/install_apk.bat
@@ -1,1 +1,1 @@
-adb install -r "D:/SOFTWARRIOR/KingsAndEmperorsOfRussia/Android/KingsAndEmperorsOfRussia/release/KingsAndEmperorsOfRussia_1_0_0_1.apk"
+adb install -r "D:/SOFTWARRIOR/KingsAndEmperorsOfRussia/Android/KingsAndEmperorsOfRussia/release/KingsAndEmperorsOfRussia_1_1_0_1.apk"
