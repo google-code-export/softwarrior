c13783bff5c3f9ae208cf428c0d1293bf986125a
wise4man@gmail.com <wise4man@gmail.com@7157628a-559a-e2cc-37fc-09edb6edd1f7>
2013-01-15 15:02:48 +0000

    changed kinoafisha

--- trunk/NoNaMeClubDownloader/Android/NoNaMeClubDownloader/src/com/softwarrior/rutrackerdownloaderlite/CustomTitleActive.java
+++ trunk/NoNaMeClubDownloader/Android/NoNaMeClubDownloader/src/com/softwarrior/rutrackerdownloaderlite/CustomTitleActive.java
@@ -5,16 +5,20 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class CustomTitleActive extends Activity {
 	protected TextView m_RightText;
 	protected TextView m_LeftText;
+	protected TextView m_CenterText;
+	protected ProgressBar m_ProgressBarCenter;
 	
 	public void OnClickHomeHandler(View v){
 		DownloaderApp.MainScreen(this);
 	}
-
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
@@ -23,5 +27,17 @@ public class CustomTitleActive extends Activity {
 	        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.custom_title);
 	        m_RightText = (TextView)findViewById(R.id.right_text);
 	        m_LeftText = (TextView)findViewById(R.id.left_text);
+	        m_CenterText = (TextView)findViewById(R.id.center_text);
+	        m_ProgressBarCenter = (ProgressBar)findViewById(R.id.progressBarCenter);
 	}
+	
+	protected void VisibleCenter(){
+		RelativeLayout viewCenter = (RelativeLayout)findViewById(R.id.ViewCenter);
+		viewCenter.setVisibility(View.VISIBLE);		
+	}
+	
+	protected void VisibilityProgressBarCenter(int visibility){
+		m_ProgressBarCenter.setVisibility(visibility);		
+	}
+	
 }
